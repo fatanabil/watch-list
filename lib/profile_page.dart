7c5b2911b1d7c4f046ad9af6d3ba2b6e3451505b
fatanabil <fatanabil63@gmail.com>
2022-02-05 00:37:34 +0700

    create profile page

---
+++ lib/profile_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:movie_list/size.dart';
+import 'package:movie_list/theme.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: dBlue,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.only(top: 24),
+          width: Size.screenWidth,
+          height: Size.screenHeight,
+          child: Column(
+            children: [
+              Text(
+                'Your Profile',
+                style: menuTitle.copyWith(fontSize: 32),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
