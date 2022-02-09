335add61f3e27f503b728e1ec2e581c7fd0a445d
fatanabil <fatanabil63@gmail.com>
2022-02-09 17:53:00 +0700

    add movie attr for model

--- lib/models/MovieModel.dart
+++ lib/models/MovieModel.dart
@@ -1,11 +1,19 @@
 class MovieModel {
-  final String movieId;
+  final String movieId, movieTitle, moviePoster;
+  final int isWatched = 0;
 
-  MovieModel({required this.movieId});
+  MovieModel({
+    required this.movieId,
+    required this.movieTitle,
+    required this.moviePoster,
+  });
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'movieId': movieId,
+      'movieTitle': movieTitle,
+      'moviePoster': moviePoster,
+      'isWatched': isWatched,
     };
   }
 }
