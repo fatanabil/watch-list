2b130973b583a90bd9f7011811196cec80e0a540
fatanabil <fatanabil63@gmail.com>
2022-02-09 17:53:13 +0700

    change db and table name

--- lib/helpers/dbhelper.dart
+++ lib/helpers/dbhelper.dart
@@ -9,31 +9,34 @@ import 'dart:async';
 class MovieDbProvider {
   Future<Database> init() async {
     Directory directory = await getApplicationDocumentsDirectory();
-    final path = join(directory.path, "moviesList.db");
+    final path = join(directory.path, "watchList.db");
 
-    return await openDatabase(path, version: 1,
-        onCreate: (Database db, int version) async {
-      await db.execute("""CREATE TABLE movie_list(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        movieID TEXT
-      )""");
-    });
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute(
+            'CREATE TABLE Watch_list (id INTEGER PRIMARY KEY AUTOINCREMENT, movieID TEXT, movieTitle TEXT, moviePoster TEXT, isWatched INTEGER DEFAULT 0)');
+      },
+    );
   }
 
   Future<int> addItem(MovieModel item) async {
     final db = await init();
 
-    return db.insert('movie_list', item.toMap(),
+    return db.insert('Watch_list', item.toMap(),
         conflictAlgorithm: ConflictAlgorithm.ignore);
   }
 
   Future<List<MovieModel>> fetchMovieList() async {
     final db = await init();
-    final maps = await db.query("movie_list");
+    final maps = await db.query("Watch_list");
 
     return List.generate(maps.length, (i) {
       return MovieModel(
         movieId: maps[i]['movieID'].toString(),
+        movieTitle: maps[i]['movieTitle'].toString(),
+        moviePoster: maps[i]['moviePoster'].toString(),
       );
     });
   }
@@ -41,10 +44,14 @@ class MovieDbProvider {
   Future<List<MovieModel>> getMovieById(String movieID) async {
     final db = await init();
     final maps = await db
-        .query('movie_list', where: "movieID = ?", whereArgs: [movieID]);
+        .query('Watch_list', where: "movieID = ?", whereArgs: [movieID]);
 
     return List.generate(maps.length, (i) {
-      return MovieModel(movieId: maps[i]['movieID'].toString());
+      return MovieModel(
+        movieId: maps[i]['movieID'].toString(),
+        movieTitle: maps[i]['movieTitle'].toString(),
+        moviePoster: maps[i]['moviePoster'].toString(),
+      );
     });
   }
 
@@ -52,7 +59,7 @@ class MovieDbProvider {
     final db = await init();
 
     int result = await db
-        .delete('movie_list', where: "movieID = ?", whereArgs: [movieID]);
+        .delete('Watch_list', where: "movieID = ?", whereArgs: [movieID]);
 
     return result;
   }
